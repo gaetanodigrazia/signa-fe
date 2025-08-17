cb558127a7d4b0851d03de69a24f3f1452f1cf95
gaetanodigrazia <education.digrazia@gmail.com>
2025-08-17 12:20:54 +0200

    Reverted api url

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,3 @@
-//export const API_BASE_URL = 'https://signa-be.onrender.com/api/v1';
+export const API_BASE_URL = 'https://signa-be.onrender.com/api/v1';
 
-export const API_BASE_URL = 'http://localhost:8080/api/v1';
+//export const API_BASE_URL = 'http://localhost:8080/api/v1';
